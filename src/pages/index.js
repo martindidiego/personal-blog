657c8cd8a56be4e3d3e5fe5d36069d5901fee1ee
Martin Di Diego <didiegomartin@gmail.com>
2019-05-27 13:12:33 -0400

    Remove useless import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-// Queries.
-import { qHome } from "./home.query"
-
 // Components.
 import { Bio } from "../components/Bio/Bio"
 import Layout from "../components/layout"
